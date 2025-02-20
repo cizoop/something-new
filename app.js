b80edb32e1761eb1a65cf198b34bc70a28d95a7b
cizoop <riyadevi0310@gmail.com>
2025-02-20 22:41:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let divs=document.querySelectorAll('.inner div');
-function color(){
+function color(){ 
+ //universal palette
  /* let red=Math.floor(Math.random()*255);
   let green=Math.floor(Math.random()*255);
   let blue=Math.floor(Math.random()*255);
   
- // Set red and blue to higher values to generate a pink shade
+ // Set red and blue to higher values to generate a pink shade//pink palette
  let red = Math.floor(Math.random() * 100) + 155; // Red should be between 155 and 255
  let blue = Math.floor(Math.random() * 100) + 155; // Blue should be between 155 and 255
  let green = Math.floor(Math.random() * 100); // Green should be between 0 and 99
- */
+ *///blue palette
  let blue = Math.floor(Math.random() * 100) + 155; // Blue should be between 155 and 255
   let red = Math.floor(Math.random() * 100); // Red should be between 0 and 99
   let green = Math.floor(Math.random() * 100)
